497172918655c13d12c5e8d3eba6d82a4839cd40
MAHARSHI025 <maharshipatel3851@gmail.com>
2025-04-22 11:27:10 +0530

    speed analysis

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Auther from "./components/Auther";
 import Socket from "./components/Socket";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 export default async function Home() {
@@ -12,6 +13,7 @@ export default async function Home() {
       <div className="mt-20">
         {/* <Auther /> */}
         <Analytics />
+        <SpeedInsights/>
         <Socket />
       </div>
     </>
